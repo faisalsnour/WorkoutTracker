246d9958ae4cb16f6a444ff8ec847b23f259cd79
Faisal Nour <faisalskhalifa@gmail.com>
2021-03-09 21:18:49 -0500

    server file has been added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+// const apirouter = require('./app/router')
+
+
+
+const PORT = process.env.PORT || 8088
+
+// will share any static html files with the browser
+app.use(express.static('public'))
+// accept incoming POST requests
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// apirouter(app)
+
+// Listener ==================================================
+app.listen(PORT, function () {
+    console.log(`Serving content on http://localhost:${PORT}`)
+})
