cef114f8e4d02ef04f45478718cd5dce2e498cf7
Faisal Nour <faisalskhalifa@gmail.com>
2021-03-15 22:27:04 -0400

    updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
